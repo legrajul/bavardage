c41c0fd7af5aebab85a0af9baa509d287a37e548
Julien Legras <julien.legras@etu.univ-rouen.fr>
2013-04-03 08:56:28 +0200

    Ajout du dossier de gestion de la base de données côté serveur sécurisé

---
+++ src/common_server/mysqlite.c
@@ -0,0 +1,123 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sqlite3.h>
+#include "mysqlite.h"
+
+
+// variable globale
+sqlite3 *database;
+sqlite3_stmt *stmt;
+
+
+/** connection la base de donnees **/
+int connect_server_database(const char *fileDb) {
+
+    // creation de la base de donnÃ©e si existe pas sinon ouverture de la base
+    int open = sqlite3_open(fileDb, &database);
+    if (open != SQLITE_OK) {
+        perror("Database connection failed\n");
+        sqlite3_close(database);
+        return -1;
+    }
+
+    //Suppression des utilisateurs dans la base de donnÃ©es
+    char delete[QUERY_SIZE] = "";
+    sprintf (delete, "DELETE FROM  users");
+
+    int sql = sqlite3_exec(database, delete, 0, 0, 0);
+    if (sql != SQLITE_OK) {
+        perror("Deleting users in server database failed\n");
+        return -1;
+    }
+    printf("Deleting users in server database...\n");
+
+    // creation de la table users
+    char create_table[QUERY_SIZE] = "CREATE TABLE IF NOT EXISTS users (login VARCHAR(20) unique, is_connected boolean)";
+    int sq = sqlite3_exec(database, create_table, 0, 0, 0);
+    if (sq != SQLITE_OK) {
+        perror("Can't create table users\n");
+        return -1;
+    }
+
+    return 1;
+}
+
+
+/** fermer la base de donnÃ©es */
+int close_server_database() {
+
+    sqlite3_close(database);
+    return 1;
+}
+
+
+/** ajoute un user dans la base **/
+int add_user(char *login) {
+
+    // creation de la requete insertion
+    char insert[QUERY_SIZE] = "";
+    sprintf (insert, "INSERT INTO users values (\'%s, true\')", login);
+
+    int sql = sqlite3_exec(database, insert, 0, 0, 0);
+    if (sql != SQLITE_OK) {
+        perror("Can't insert in server database");
+        return -1;
+    } else {
+        printf("Added user %s succesfully\n", login);
+    }
+    return 1;
+}
+
+
+/** supprime un user de la base **/
+int delete_user (char *login) {
+
+    char delete[QUERY_SIZE] = "";
+    sprintf (delete, "DELETE FROM users WHERE login = \'%s\'", login);
+
+    int sql = sqlite3_exec(database, delete, 0, 0, 0);
+    if (sql != SQLITE_OK) {
+        perror("Can't check in server database\n");
+        return -1;
+    } else {
+        printf("User delete succesfully\n");
+    }
+
+    return 1;
+}
+
+
+/** verifie la prÃ©sence d'un user */
+int check_user(char *login) {
+    char select[QUERY_SIZE] = "";
+    sprintf (select, "SELECT * FROM users WHERE login = \'%s\'", login);
+
+    int sql = sqlite3_prepare_v2(database, select, -1, &stmt, 0);
+    if (sql) {
+        perror("Cant select in server database\n");
+        return -1;
+    } else {
+        int fetch = 1;
+        int total = 0;
+        while(fetch) {
+            switch (sqlite3_step(stmt)) {
+            case SQLITE_ROW:
+                total++;
+                break;
+            case SQLITE_DONE:
+                fetch = 0;
+                break;
+            default:
+                printf("Some error encountered\n");
+                return -1;
+            }
+        }
+        if (total == 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
---
+++ src/common_server/mysqlite.h
@@ -0,0 +1,40 @@
+#ifndef _MYSQLITE_H
+#define _MYSQLITE_H
+
+  #define QUERY_SIZE 524
+
+  // commun au serveurs
+  
+/**
+ *   Connecte le server Ã  la base de donnÃ©es
+ *   @return 1 si la connexion s'est bien effectuÃ©e, -1 sinon
+ */
+  int connect_server_database(const char *fileDb);
+  
+/**
+ *   Ajoute un utilisateur dans la base de donnÃ©es
+ *	 @param login 
+ *   @return 1 si la l'ajout s'est bien effectuÃ©, -1 sinon
+ */
+  int add_user(char *login);
+  
+/**
+ *   Supprime un utilisateur dans la base de donnÃ©es
+ *	 @param login 
+ *   @return 1 si la suppression s'est bien effectuÃ©e, -1 sinon
+ */
+  int delete_user(char *login);
+  
+/**
+ *   VÃ©rifie si un utilisateur est dans la base de donnÃ©es
+ *	 @param login 
+ *   @return 1 si la vÃ©rification s'est bien effectuÃ©e, -1 sinon
+ */
+  int check_user(char *login);
+  
+/**
+ *   DÃ©connecte le serveur de la base de donnÃ©es
+ */  
+  int close_server_database();
+
+#endif
